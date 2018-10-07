4efbb7ca47c2699fb41dba2cb5f8b5119b0af7d3
dani <dbalchev@gmail.com>
2018-10-07 09:13:34 +0300

    Implement authentication mechanism

--- src/index.js
+++ src/index.js
@@ -13,34 +13,56 @@ class MainComponent extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            google: null
+            google: null,
+            loggedIn: false,
         }
         this.initGoogle()
     }
     async initGoogle() {
         const google = await initGoogle()
-        this.setState({google})
+        google.authInstance.isSignedIn.listen(loggedIn => this.setState({loggedIn}))
+        this.setState({
+            google,
+            loggedIn: google.authInstance.isSignedIn.get()})
     }
     renderComponent(ComponentClass) {
         return props => (<ComponentClass {...props} google={this.state.google} />)
     }
+    renderLoggedInComponents() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/' render={this.renderComponent(NotLogged)}/>
+                    <Route exact path='/logged' render={this.renderComponent(Logged)}/>
+                    <Route exact path='/graph/:spreadsheetId' render={this.renderComponent(Graph)}/>
+                </Switch>
+            </BrowserRouter>
+        )
+    }
     render() {
         if (this.state.google === null) {
             return (<div> Please, wait a bit untill Google loads</div>)
-        }    
+        }
+        if (!this.state.loggedIn) {
+            return (
+                <input type="button" value="sign in" disabled={this.state.disabled} 
+                    onClick={() => this.state.google.authInstance.signIn()}/>
+            )
+        }
         return (
-            <Switch>
-                <Route exact path='/' render={this.renderComponent(NotLogged)}/>
-                <Route exact path='/logged' render={this.renderComponent(Logged)}/>
-                <Route exact path='/graph/:spreadsheetId' render={this.renderComponent(Graph)}/>
-            </Switch>
+            <div>
+                <div>
+                <input type="button" value="sign out" disabled={this.state.disabled} 
+                    onClick={() => this.state.google.authInstance.signOut()}/>
+                </div>
+                {this.renderLoggedInComponents()}
+            </div>
         )
     }
+
 
 }
 
 ReactDOM.render(
-    <BrowserRouter>
-        <MainComponent/>
-    </BrowserRouter>, document.getElementById('root'));
+    <MainComponent/>, document.getElementById('root'));
 registerServiceWorker();
